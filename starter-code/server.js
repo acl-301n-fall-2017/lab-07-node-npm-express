10490d395c0f93285a1ea6308d307e5e1dcba3d2
cruzayye <johnnyc010392@gmail.com>
2018-10-28 13:13:42 -0700

    setup static resources as an argument as well as stretch goal and console logged port

--- starter-code/server.js
+++ starter-code/server.js
@@ -15,6 +15,13 @@ const PORT = process.env.PORT || 3000;
 
 // TODO: Include all of the static resources as an argument to app.use()
 
+app.use(express.static('./public'));
+
+app.get('/new', function(request, response){
+  response.sendFile('./public/index.html');
+
+})
+
 // TODO: (STRETCH) Write a new route that will handle a request and send the new.html file back to the user
 
 
@@ -22,10 +29,12 @@ app.post('/articles', bodyParser, function(request, response) {
   // REVIEW: This route will receive a new article from the form page, new.html,
   // and log that form data to the console. We will wire this up soon to actually
   // write a record to our persistence layer!
+  
   console.log(request.body);
   response.send('Record posted to server!!');
 })
 
 app.listen(PORT, function() {
   // TODO: Log to the console a message that lets you know which port your server has started on
+  console.log(`listening on ${PORT}`);
 });
