39891ee01d794a4b1025a19ed0a6aab3cd5edc99
Jon Washington <jonathan.washington@gmail.com>
2018-10-31 19:34:56 -0700

    added route to /index.html

--- starter-code/server.js
+++ starter-code/server.js
@@ -17,19 +17,21 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.static('./public'));
 
-app.get('/new', function(request, response){
-  response.sendFile('./public/index.html');
+app.get('/', function(request, response){
+  response.sendFile('./index.html');
+});
 
-})
+app.get('/new', function(request, response){
+  response.sendFile('new.html', {root: './public'});
+});
 
 // TODO: (STRETCH) Write a new route that will handle a request and send the new.html file back to the user
-
 
 app.post('/articles', bodyParser, function(request, response) {
   // REVIEW: This route will receive a new article from the form page, new.html,
   // and log that form data to the console. We will wire this up soon to actually
   // write a record to our persistence layer!
-  
+
   console.log(request.body);
   response.send('Record posted to server!!');
 })
